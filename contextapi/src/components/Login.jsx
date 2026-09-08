c40fbb88e1a3b2d6ce79029ab08981902e41d884
kiran-k-k <kendrek57@gmail.com>
2026-09-08 17:12:49 +0530

    context api project adding

---
+++ contextapi/src/components/Login.jsx
@@ -0,0 +1,32 @@
+import React, {useState, useContext} from 'react'
+import UserContext from '../context/userContext'
+
+function Login() {
+    const [userName, setUserName] = useState('');
+    const [password, setPassword] = useState('');
+
+    const {setUser} = useContext(UserContext);
+
+    const handelSubmit= (e) =>{
+        e.preventDefault()
+        setUser({userName, password})
+    }
+  return (
+    <div>
+        <h2>Login</h2>
+        <input type="text"
+        value={userName}
+        onChange={(e) => setUserName(e.target.value)}
+        placeholder='UserName' />
+        <input type="text" 
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        placeholder='Password' />
+        <button onClick={handelSubmit}>Submit</button>
+
+
+    </div>
+  )
+}
+
+export default Login
